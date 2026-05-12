5b6ce3ac799c26712cba1d5fb2d56378ef0397fe
재욱 <haensol976520@gmail.com>
2026-05-12 20:24:33 +0900

    서버 설정에 프리뷰 호스트 추가

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
     proxy: {
       '/api': {
@@ -10,5 +11,9 @@ export default defineConfig({
         changeOrigin: true,
       },
     },
+  },
+
+  preview: {
+    allowedHosts: ['ssh.gsmsv.site'],
   },
 });
